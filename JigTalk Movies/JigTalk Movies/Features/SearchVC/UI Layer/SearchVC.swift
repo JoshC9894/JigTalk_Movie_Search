13758f8b6994a810647493ca3baff1de05053862
Joshua Colley <>
2020-01-22 20:29:56 +0000

    Setup project: Create files and folder structure for search view controller

---
+++ JigTalk Movies/JigTalk Movies/Features/SearchVC/UI Layer/SearchVC.swift
@@ -0,0 +1,24 @@
+//
+//  SearchVC.swift
+//  JigTalk Movies
+//
+//  Created by Joshua Colley on 22/01/2020.
+//  Copyright © 2020 Joshua Colley. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchViewProtocol: class {
+}
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Implement SearchViewProtocol
+extension SearchVC: SearchViewProtocol {
+    
+}
